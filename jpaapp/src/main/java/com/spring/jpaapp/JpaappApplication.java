6a1d5a09848d735548c63609391071a69708d56b
ParkSungGyu1 <steven0507@naver.com>
2022-06-06 09:53:49 +0900

    프로젝트 생성 - 웹 어플리케이션 개발

---
+++ jpaapp/src/main/java/com/spring/jpaapp/JpaappApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaappApplication.class, args);
+	}
+
+}
