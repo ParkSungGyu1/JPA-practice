1d97d4e2160beb156fad727e0f287fbb0258f828
ParkSungGyu1 <steven0507@naver.com>
2022-06-15 22:56:49 +0900

    플랫 쿼리 작성

--- jpaapp/src/main/java/com/spring/jpaapp/api/OrderApiController.java
+++ jpaapp/src/main/java/com/spring/jpaapp/api/OrderApiController.java
@@ -6,6 +6,10 @@ import com.spring.jpaapp.model.OrderItem;
 import com.spring.jpaapp.model.OrderStatus;
 import com.spring.jpaapp.repository.OrderRepository;
 import com.spring.jpaapp.repository.OrderSearch;
+import com.spring.jpaapp.repository.order.query.OrderFlatDto;
+import com.spring.jpaapp.repository.order.query.OrderItemQueryDto;
+import com.spring.jpaapp.repository.order.query.OrderQueryDto;
+import com.spring.jpaapp.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +20,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -62,6 +70,31 @@ public class OrderApiController {
                 .collect(Collectors.toList());
         return collect;
     }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    /*@GetMapping("/api/v6/orders")
+    public List<OrderFlatDto> ordersV6(){
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+    }*/
 
     @Data
     static class OrderDto{
